03fa4e6827be5020a1fd4159be2ad92a362efe46
Brady Bott <bottbrady@gmail.com>
2021-08-17 13:20:24 -0600

    fixed rollbar issues

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = process.env.PORT || 4040
+const port = process.env.PORT || 6543
 
 // include and initialize the rollbar library with your access token
 // include and initialize the rollbar library with your access token
@@ -9,11 +9,12 @@ var Rollbar = require("rollbar");
 var rollbar = new Rollbar({
   accessToken: '9efeea872d3a4e6d9bfa472df5dc6b75',
   captureUncaught: true,
-  captureUnhandledRejections: true
+  captureUnhandledRejections: true,
+  endpoint: "https://api.rollbar.com/api/1/item",
 });
 
 // record a generic message and send it to Rollbar
-rollbar.log("Hello world!");
+
 
 
 
@@ -21,10 +22,17 @@ app.use(express.json())
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
-    rollbar.log("Hello Brady, Dominator of Code.");
-    doAThing()
+    rollbar.log("Hello world!");
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
+app.get('/thing', (req, res) => {
+    try {
+        doTheTHing()
+    } catch(err) {
+        console.log(err)
+        rollbar.error(err)
+    }
+})
 
 app.listen(port, () => console.log(`Number ${port}, it is time for your deconstruction. Please procede to the engine bay.`))
