7cef23035a6a8376ca75c538fba5ee74828c6652
Brady Bott <bottbrady@gmail.com>
2021-08-17 12:35:01 -0600

    added rollbar code to server

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(express.json())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
     rollbar.log("Hello Brady, Dominator of Code.");
+    doAThing()
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
